c4139c2de7d57af73a74e9ba5d9d5af11ed91d2b
Nitipaht Jumnongnok <jumnongnok@gmail.com>
2018-04-25 01:57:14 +0700

    Add files via upload

---
+++ AppMan/ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+            string b = "";
+            string c = "";
+            string d = "";
+            string e = "";
+            Console.WriteLine("By Mr.Nitiphat Jumnongnok");
+            Console.WriteLine("=================Formula==============");
+            Console.WriteLine("Enter Number 1 To Case1 (A*B)+C");
+            Console.WriteLine("Enter Number 2 To Case2 (a*b+c)/d");
+            Console.WriteLine("Enter Number 3 To Case3 (a-b+c/d*e)+f");
+            Console.Write("Enter Number = ");
+            int caseWs = int.Parse(Console.ReadLine());
+
+            switch (caseWs)
+            {
+                case 1:
+                    string formulaC1 = "(a*b)+c";
+                    Console.WriteLine("Case1 = (A*B)+C");
+
+                    Console.WriteLine("Enter Num A:");
+                    a = Console.ReadLine();
+                    Console.WriteLine("Enter Num B:");
+                    b = Console.ReadLine();
+                    Console.WriteLine("Enter Num C:");
+                    c = Console.ReadLine();
+
+                    formulaC1 = formulaC1.Replace("a", a).Replace("b", b).Replace("c", c);
+
+                    Console.WriteLine("Formula: {0}\tResult: {1}", formulaC1, Evaluate(formulaC1));
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    string formulaC2 = "(a*b+c)/d";
+                    Console.WriteLine("Case2 = (A*B+C)/D");
+
+                    Console.WriteLine("Enter Num A:");
+                    a = Console.ReadLine();
+                    Console.WriteLine("Enter Num B:");
+                    b = Console.ReadLine();
+                    Console.WriteLine("Enter Num C:");
+                    c = Console.ReadLine();
+                    Console.WriteLine("Enter Num D:");
+                    d = Console.ReadLine();
+
+                    formulaC2 = formulaC2.Replace("a", a).Replace("b", b).Replace("c", c).Replace("d",d);
+
+                    Console.WriteLine("Formula: {0}\tResult: {1}", formulaC2, Evaluate(formulaC2));
+                    Console.ReadLine();
+                    break;
+                case 3:
+                    string formulaC3 = "(a-b+c/d*e)";
+                    Console.WriteLine("Case3 = (A-B+C/D*E)");
+
+                    Console.WriteLine("Enter Num A:");
+                    a = Console.ReadLine();
+                    Console.WriteLine("Enter Num B:");
+                    b = Console.ReadLine();
+                    Console.WriteLine("Enter Num C:");
+                    c = Console.ReadLine();
+                    Console.WriteLine("Enter Num D:");
+                    d = Console.ReadLine();
+                    Console.WriteLine("Enter Num E:");
+                    e = Console.ReadLine();
+
+                    formulaC3 = formulaC3.Replace("a", a).Replace("b", b).Replace("c", c).Replace("d", d).Replace("e",e);
+
+                    Console.WriteLine("Formula: {0}\tResult: {1}", formulaC3, Evaluate(formulaC3));
+                    Console.ReadLine();
+
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+        }
+        public static double Evaluate(string expression)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("myExpression", string.Empty.GetType(), expression);
+            DataRow row = dt.NewRow();
+            dt.Rows.Add(row);
+            return double.Parse((string)row["myExpression"]);
+        }
+    }
+}
---
+++ AppMan/ConsoleApp4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum  = 0;
+            int[] input = new int[25];
+            int[] win1 = new int[] { 3, 4, 8, 13, 18, 19, 23 }; //bingo
+            int[] win2 = new int[] { 2, 1, 12, 15, 6, 18, 16, 4, 3, 21, 11 }; //bingo
+            Console.WriteLine("By Mr.Nitiphat Jumnongnok");
+            Console.WriteLine("=================Bingo==============");
+            for (int i = 0; i <= input.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine("Input your number : " + i);
+                    int numBingo = int.Parse(Console.ReadLine());
+                    input[i] = numBingo;
+                }
+
+                catch
+                {
+                    Console.WriteLine("Invalid Input");
+                    continue;
+                }
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (win1.Contains(input[i]))
+                {
+                    Console.Write("[YOU BINGOooooooo]");
+                }
+                else if(win2.Contains(input[i]))
+                {
+                    Console.Write("[YOU BINGOooooooo]");
+                }
+                else
+                {
+                    Console.Write("[YOU LOSEeeeeeeee]");
+                }
+            }
+            Console.WriteLine();
+
+            foreach (int num in input)
+            {
+                Console.Write(" {0}", num);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
